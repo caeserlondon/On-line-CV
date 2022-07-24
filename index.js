b758c17a3fc437ffafa736e41f14095e5c57a5c8
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-24 21:21:27 +0100

    Cleaning up

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const PORT = process.env.PORT || 3000
-
-// set static folder
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.listen(PORT, () => console.log('Server is listening on port ' + PORT))
